352ccf16ff72ad271a08f000daeec587cf9cfa98
Rafael Pires <rafael.pires.sp@gmail.com>
2013-04-19 23:34:01 -0300

    adiciona gotas no header e tira ele de dentro do container para que pegue toda a largura da pagina

--- wp-content/themes/pongstrap/header.php
+++ wp-content/themes/pongstrap/header.php
@@ -26,91 +26,95 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
-		<div class="container">
-			<div id="page" class="hfeed row">
-				<?php tha_header_before(); ?>
-				<header id="branding" role="banner" class="span12">
-					<?php tha_header_top();
-					wp_nav_menu( array(
-						'container'			=>	'nav',
-						'container_class'	=>	'subnav clearfix',
-						'theme_location'	=>	'header-menu',
-						'menu_class'		=>	'nav nav-pills pull-right',
-						'depth'				=>	3,
-						'fallback_cb'		=>	false,
-						'walker'			=>	new The_Bootstrap_Nav_Walker,
-					) ); ?>
-					<div class="row">					
-						<div id="cdi-logo" class="span9">						
-							<h1 id="site-title">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-									<!--span><?php bloginfo( 'name' ); ?></span-->
-									<img src="<?php bloginfo('template_directory'); ?>/img/cdi_logo.png" alt="Logotipo da cdi" height=162 width=432>
-								</a>
-							</h1>
-						</div>
-						
-						<div class="cdi-redes-sociais span3">
-							<div class="row">
-							<!--h2 id="site-description"><?php bloginfo( 'description' ); ?></h2-->
-								<a target="_blank" href="<?php bloginfo(url); ?>/feed"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/rss.jpg" alt="Assine nosso RSS" height=38 width=38></a>
-								<a target="_blank" href="http://www.youtube.com/user/redecdi"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/youtube.jpg" alt="Visite nosso canal no Youtube" height=38 width=38></a>
-								<a target="_blank" href="https://twitter.com/ongCDI"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/twitter.jpg" alt="Visite nosso Twitter" height=38 width=38></a>
-								<a target="_blank" href="http://www.facebook.com/pageongcdi"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/facebook.jpg" alt="Visite nosso Facebook" height=38 width=38></a>
-							</div>
-							<div class='row'>
-								<a href="#cdi_Modal_doacao" data-toggle="modal" class='pull-right cdi_doacao'>
-									<img src='<?php bloginfo('template_directory'); ?>/img/donation.png' />
-								</a>
-							</div>
+		<?php tha_header_before(); ?>
+		<header id="branding" role="banner">
+			<?php tha_header_top();
+			wp_nav_menu( array(
+				'container'			=>	'nav',
+				'container_class'	=>	'subnav clearfix',
+				'theme_location'	=>	'header-menu',
+				'menu_class'		=>	'nav nav-pills pull-right',
+				'depth'				=>	3,
+				'fallback_cb'		=>	false,
+				'walker'			=>	new The_Bootstrap_Nav_Walker,
+			) ); ?>
+			<div class='container'>
+			<div class="row">					
+				<div id="cdi-logo" class="span9">						
+					<h1 id="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+							<!--span><?php bloginfo( 'name' ); ?></span-->
+							<img src="<?php bloginfo('template_directory'); ?>/img/cdi_logo.png" alt="Logotipo da cdi" height=162 width=432>
+						</a>
+					</h1>
+				</div>
+				
+				<div class="cdi-redes-sociais span3">
+					<div class="row">
+					<!--h2 id="site-description"><?php bloginfo( 'description' ); ?></h2-->
+						<a target="_blank" href="<?php bloginfo(url); ?>/feed"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/rss.jpg" alt="Assine nosso RSS" height=38 width=38></a>
+						<a target="_blank" href="http://www.youtube.com/user/redecdi"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/youtube.jpg" alt="Visite nosso canal no Youtube" height=38 width=38></a>
+						<a target="_blank" href="https://twitter.com/ongCDI"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/twitter.jpg" alt="Visite nosso Twitter" height=38 width=38></a>
+						<a target="_blank" href="http://www.facebook.com/pageongcdi"><img class="pull-right" src="<?php bloginfo('template_directory'); ?>/img/facebook.jpg" alt="Visite nosso Facebook" height=38 width=38></a>
+					</div>
+					<div class='row'>
+						<a href="#cdi_Modal_doacao" data-toggle="modal" class='pull-right cdi_doacao'>
+							<img src='<?php bloginfo('template_directory'); ?>/img/donation.png' />
+						</a>
+					</div>
 
+				</div>
+			</div>
+			<?php if ( get_header_image() ) : ?>
+			<a id="header-image" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
+			</a>
+			<?php endif; // if ( get_header_image() ) ?>
+			</div>
+
+			<nav id="access" role="navigation">
+				<h3 class="assistive-text"><?php _e( 'Main menu', 'the-bootstrap' ); ?></h3>
+				<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'the-bootstrap' ); ?>"><?php _e( 'Skip to primary content', 'the-bootstrap' ); ?></a></div>
+				<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'the-bootstrap' ); ?>"><?php _e( 'Skip to secondary content', 'the-bootstrap' ); ?></a></div>
+				<?php if ( has_nav_menu( 'primary' ) OR the_bootstrap_options()->navbar_site_name OR the_bootstrap_options()->navbar_searchform ) : ?>
+				<div <?php the_bootstrap_navbar_class(); ?>>
+					<div class="navbar-inner">
+						<div class="container">
+							<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+							<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</a>
+							<?php if ( the_bootstrap_options()->navbar_site_name ) : ?>
+							<span class="brand"><?php bloginfo( 'name' ); ?></span>
+							<?php endif;?>
+							<div class="nav-collapse">
+								<?php wp_nav_menu( array(
+									'theme_location'	=>	'primary',
+									'menu_class'		=>	'nav',
+									'depth'				=>	3,
+									'fallback_cb'		=>	false,
+									'walker'			=>	new The_Bootstrap_Nav_Walker,
+								) ); 
+								if ( the_bootstrap_options()->navbar_searchform ) {
+									the_bootstrap_navbar_searchform();
+								} ?>
+						    </div>
 						</div>
 					</div>
-					<?php if ( get_header_image() ) : ?>
-					<a id="header-image" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-					</a>
-					<?php endif; // if ( get_header_image() ) ?>
+				</div>
+				<?php endif; ?>
+			</nav><!-- #access -->
+			<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
+				yoast_breadcrumb( '<nav id="breadcrumb" class="breadcrumb">', '</nav>' );
+			}
+			tha_header_bottom(); ?>
+		</header><!-- #branding -->
 
-					<nav id="access" role="navigation">
-						<h3 class="assistive-text"><?php _e( 'Main menu', 'the-bootstrap' ); ?></h3>
-						<div class="skip-link"><a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to primary content', 'the-bootstrap' ); ?>"><?php _e( 'Skip to primary content', 'the-bootstrap' ); ?></a></div>
-						<div class="skip-link"><a class="assistive-text" href="#secondary" title="<?php esc_attr_e( 'Skip to secondary content', 'the-bootstrap' ); ?>"><?php _e( 'Skip to secondary content', 'the-bootstrap' ); ?></a></div>
-						<?php if ( has_nav_menu( 'primary' ) OR the_bootstrap_options()->navbar_site_name OR the_bootstrap_options()->navbar_searchform ) : ?>
-						<div <?php the_bootstrap_navbar_class(); ?>>
-							<div class="navbar-inner">
-								<div class="container">
-									<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-									<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</a>
-									<?php if ( the_bootstrap_options()->navbar_site_name ) : ?>
-									<span class="brand"><?php bloginfo( 'name' ); ?></span>
-									<?php endif;?>
-									<div class="nav-collapse">
-										<?php wp_nav_menu( array(
-											'theme_location'	=>	'primary',
-											'menu_class'		=>	'nav',
-											'depth'				=>	3,
-											'fallback_cb'		=>	false,
-											'walker'			=>	new The_Bootstrap_Nav_Walker,
-										) ); 
-										if ( the_bootstrap_options()->navbar_searchform ) {
-											the_bootstrap_navbar_searchform();
-										} ?>
-								    </div>
-								</div>
-							</div>
-						</div>
-						<?php endif; ?>
-					</nav><!-- #access -->
-					<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
-						yoast_breadcrumb( '<nav id="breadcrumb" class="breadcrumb">', '</nav>' );
-					}
-					tha_header_bottom(); ?>
-				</header><!-- #branding --><?php
+		<div class="container">
+			<div id="page" class="hfeed row">
+				<?php
 				tha_header_after();
 				
 
