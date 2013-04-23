040308549431a69666c54f64640a644a6425d68a
Rafael Pires <rafael.pires.sp@gmail.com>
2013-04-23 12:25:41 -0300

    IMPORTANTE :: a pasta content-wp tem todos os htmls que devem ser copiados e colados diretamente dentro do wordpress na página com o mesmo nome

---
+++ wp-content/themes/pongstrap/content-wp/home.php
@@ -0,0 +1,53 @@
+<div class="row-fluid" id="cdi_home">
+	<div class="span5">
+		<iframe src="http://www.youtube.com/embed/bIfT2-gLKCw" height="315" width="560" allowfullscreen="" frameborder="0"></iframe>
+	</div>
+	<div class="span7">
+		<h3 class='cdi_azul'>CDI & Nossas conquistas</h3>
+		<p>O CDI completou 18 anos de atuação. Uma história que começou com um sonho do seu fundador, Rodrigo Baggio, e virou realidade. Acreditamos no potencial do ser humano e na tecnologia como ferramenta de libertação e de empoderamento. Os desafios da inclusão digital continuam, tanto no Brasil como nos outros 11 países onde atuamos.</p>
+		<span class='cdi_cumeros'>
+			<table class='table table-condensed table-striped table-hover'>
+				<tr>
+					<td >Espaços de Inclusão Digital:</td><td class='cdi-valor'>780</td>
+				</tr><tr>
+					<td >Vidas impactadas desde 1995:</td><td class='cdi-valor'>1.54 milhão</td>
+				</tr><tr>
+					<td >Beneficiários diretos em 2012:</td><td class='cdi-valor'>92.084</td>
+				</tr><tr>
+					<td >Educadores em 2012:</td><td class='cdi-valor'>1.007</td>
+				</tr><tr>
+					<td >Países com presença CDI:</td><td class='cdi-valor'>12</td>
+				</tr>
+			</table>
+		</span>
+	</div>
+</div>
+
+<div class='row-fluid' id='cdi_home_boxes'>
+	<ul class='thumbnail'>
+		<li class='span4'>
+			<div class='thumbnails'>
+				<h3 class='cdi_verde'>Projetos</h3>
+				<img src='wp-content/themes/pongstrap/img/home-box-1.jpg' />
+				<p>O CDI desenvolve projetos especificamente para as realidades das comunidades de forma modular e complementar, podendo assim uma comunidade ter vários projetos ao mesmo tempo.</p>
+				<a href='#' class='btn btn-large btn-primary cdi_vermelho'>Lista dos projetos</a>
+			</div>
+		</li>
+		<li class='span4'>
+			<div class='thumbnails'>
+				<h3 class='cdi_azul'>CDI GLobal</h3>
+				<img src='wp-content/themes/pongstrap/img/home-box-2.jpg' />
+				<p>Em 12 países o CDI tem projetos na América Latina, Estados Unidos e Europa aplicados pelos seus 780 centros de inclusão digital.</p>
+				<a href='#' class='btn btn-large btn-primary cdi_azul'>Veja nosso mapa</a>
+			</div>
+		</li>
+		<li class='span4'>
+			<div class='thumbnails'>
+				<h3 class='cdi_vermelho'>História de sucesso</h3>
+				<img src='wp-content/themes/pongstrap/img/home-box-3.jpg' />
+				<p>Em 18 anos, o CDI colecionou diversas histórias inspiradoras de superação, colaboração e união. Histórias lindas que nos fazem ter orgulho de cada membro da rede CDI</p>
+				<a href='#' class='btn btn-large btn-primary cdi_vermelho'>Conheça mais nossa história</a>
+			</div>
+		</li>
+	</ul>
+</div>
