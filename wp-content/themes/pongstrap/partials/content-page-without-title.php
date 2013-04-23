7017bd9b04767ba1b32501754168c73670dbb051
Rafael Pires <rafael.pires.sp@gmail.com>
2013-04-23 12:24:04 -0300

    corrige template da home e partial full-width-without-title, tira o conteúdo do código passando pra dentro do conteúdo da pagina home

--- wp-content/themes/pongstrap/partials/content-page-without-title.php
+++ wp-content/themes/pongstrap/partials/content-page-without-title.php
@@ -14,36 +14,9 @@ tha_entry_before(); ?>
 	<?php tha_entry_top(); ?>
 	
 	<div class="entry-content clearfix">
-		<div class='row'>
-			<div class='span8'>
-				<?php
-				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ) );
-				the_bootstrap_link_pages(); ?>
-			</div>
-			<div class='span4'>
-				<div id='cdi_numeros'>
-					<h3>Nossas conquistas</h3>
-					<table class='table table-condensed table-striped table-hover'>
-						<tr>
-							<td >Espaços de Inclusão Digital:</td><td>780</td>
-						</tr><tr>
-							<td >Vidas impactadas desde 1995:</td><td>1.54 milhão</td>
-						</tr><tr>
-							<td >Beneficiários diretos em 2012:</td><td>92.084</td>
-						</tr><tr>
-							<td >Educadores em 2012:</td><td>1.007</td>
-						</tr><tr>
-							<td >Países com presença CDI:</td><td>12</td>
-						</tr>
-					</table>
-				</div>
-				<a href='http://www.cdiglobal.org' target='_blank' class='btn btn-large btn-block cdi_global_btn'>
-						<img src='<?php bloginfo('template_directory'); ?>/img/CDI-GLOBAL.png' class='pull-left cdi_logo_global'/>
-						<p>Visit our international site</p>
-				 </a>
-			</div>
-		</div>
-
+		<?php
+		the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ) );
+		the_bootstrap_link_pages(); ?>
 	</div><!-- .entry-content -->
 	<?php edit_post_link( __( 'Edit', 'the-bootstrap' ), '<footer class="entry-meta"><span class="edit-link label">', '</span></footer>' );
 	
---
+++ wp-content/themes/pongstrap/templates/_cdi_full_width_without_title.php
@@ -0,0 +1,31 @@
+<?php
+/** _cdi_full_width_without_title.php
+ *
+ * Template Name: full width without title
+ *
+ * @author 	Rafael D´Alessandro Pires
+ * @package The Bootstrap
+ */
+
+get_header(); ?>
+
+<section id="primary" class="span12">
+	<?php tha_content_before(); ?>
+	<div id="content" role="main">
+		<?php tha_content_top();
+		
+		the_post();
+		get_template_part( '/partials/content', 'page-without-title' );
+		comments_template( '', true );
+		
+		tha_content_bottom(); ?>
+	</div><!-- #content -->
+	<?php tha_content_after(); ?>
+</section><!-- #primary -->
+
+<?php
+get_footer();
+
+
+/* End of file _full_width.php */
+/* Location: ./wp-content/themes/the-bootstrap/_full_width.php */
